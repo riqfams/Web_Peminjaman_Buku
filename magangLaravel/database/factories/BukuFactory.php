5bb4574e672f49d9311518c3c1949734dee78a02
ariq <ariqfms012@students.unnes.ac.id>
2023-07-07 15:44:53 +0700

    nambahin faker\factory ke buku

---
+++ magangLaravel/database/factories/BukuFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Faker;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class BukuFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $faker = Faker\Factory::create();
+        return [
+            //
+        ];
+    }
+}
