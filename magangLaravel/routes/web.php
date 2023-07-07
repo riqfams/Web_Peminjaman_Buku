2ed7f1ca2611ddcabc7526bf2755b4255a83622d
ariq <ariqfms012@students.unnes.ac.id>
2023-07-07 15:42:37 +0700

    benerin route

--- magangLaravel/routes/web.php
+++ magangLaravel/routes/web.php
@@ -13,12 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('listBuku');
-});
+// Route::get('/', function () {
+//     return view('listBuku');
+// });
 
 //Route::prefix('buku')->group(function(){
-    Route::get('buku/list', 'App\Http\Controllers\BukuController@list');
+    Route::get('/', 'App\Http\Controllers\BukuController@list');
     Route::get('buku/tambah', 'App\Http\Controllers\BukuController@tambah');
     Route::get('buku/edit', 'App\Http\Controllers\BukuController@edit');
 //});
