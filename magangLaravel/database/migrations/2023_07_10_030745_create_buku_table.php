1f9bb8142054869d7e3001ab7bc76dcf3e88b810
ariq <ariqfms012@students.unnes.ac.id>
2023-07-10 17:37:54 +0700

    create and modif buku table

---
+++ magangLaravel/database/migrations/2023_07_10_030745_create_buku_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('buku', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul', 100)->required();
+            $table->string('penulis', 100)->required();
+            $table->string('penerbit', 100)->required();
+            $table->year('tahunTerbit', 4)->required();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('buku');
+    }
+};
