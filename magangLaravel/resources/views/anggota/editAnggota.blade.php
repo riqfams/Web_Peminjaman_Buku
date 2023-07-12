f43b46a17b24594ac6917253934dd91f7c19e6c9
ariq <ariqfms012@students.unnes.ac.id>
2023-07-12 17:57:49 +0700

    add view

---
+++ magangLaravel/resources/views/anggota/editAnggota.blade.php
@@ -0,0 +1,48 @@
+@extends('master')
+@section('konten')
+
+<div class="pageTitle">
+    <span>Edit Anggota</span>
+</div>
+<div class="mt-5 col-5 m-auto">
+    <form action="/anggota/update/{{ $anggota->id }}" method="post">
+        @csrf
+        @method('put')
+        <div class="mb-3">
+            <label for="nama">Nama</label>
+            <input type="text" class="form-control" name="nama" id="nama" value="{{ $anggota->nama }}" required>
+        </div>
+        <div class="mb-3">
+            <label for="kelamin">Kelamin</label>
+            <select name="kelamin" id="kelamin" class="form-control" required>
+                <option value="{{ $anggota->kelamin }}">{{ $anggota->kelamin }}</option>
+                @if ($anggota->kelamin == 'L')
+                    <option value="P">P</option>
+                @else
+                    <option value="L">L</option>
+                @endif
+            </select>
+        </div>
+        <div class="mb-3">
+            <label for="nim">NIM</label>
+            <input type="number" class="form-control" name="nim" id="nim" value="{{ $anggota->nim }}" required>
+        </div>
+        <div class="mb-3">
+            <label for="prodi">Prodi</label>
+            <select name="prodi_id" id="prodi" class="form-control" required>
+                <option value="{{ $anggota->prodi->id }}">{{ $anggota->prodi->name }}</option>
+                @foreach ($prodi as $p)
+                    <option value="{{ $p->id }}">{{ $p->name }}</option>
+                @endforeach
+            </select>
+        </div>
+        <div class="mb-3">
+            <label for="alamat">Alamat</label>
+            <input type="text" class="form-control" name="alamat" id="alamat" value="{{ $anggota->alamat }}" required>
+        </div>
+        <div class="mb-3 text-center">
+            <button type="submit" class="btn btn-success">Simpan</button>
+        </div>
+    </form>
+</div>
+@endsection
