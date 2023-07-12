3031f00422236ecb2359d2cc7fa55d3f9c8f20ad
ariq <ariqfms012@students.unnes.ac.id>
2023-07-12 17:58:41 +0700

    add route

--- magangLaravel/routes/web.php
+++ magangLaravel/routes/web.php
@@ -22,21 +22,23 @@ Route::get('/', 'App\Http\Controllers\BukuController@list');
 Route::get('/buku/list', 'App\Http\Controllers\BukuController@list');
 Route::get('/buku/detail/{id}', 'App\Http\Controllers\BukuController@detail');
 Route::get('/buku/tambah', 'App\Http\Controllers\BukuController@tambah');
-Route::get('/buku/edit', 'App\Http\Controllers\BukuController@edit');
 Route::post('/buku/store', 'App\Http\Controllers\BukuController@store');
-//Route::post('/daftarServicer/store',[PjPerbaikanController::class,'store'])->middleware('auth');
+Route::get('/buku/edit/{id}', 'App\Http\Controllers\BukuController@edit');
+Route::put('/buku/update/{id}', 'App\Http\Controllers\BukuController@update');
 
 Route::get('/anggota/list', 'App\Http\Controllers\AnggotaController@list');
 Route::get('/anggota/detail/{id}', 'App\Http\Controllers\AnggotaController@detail');
 Route::get('/anggota/tambah', 'App\Http\Controllers\AnggotaController@tambah');
 Route::post('/anggota/store', 'App\Http\Controllers\AnggotaController@store');
-
+Route::get('/anggota/edit/{id}', 'App\Http\Controllers\AnggotaController@edit');
+Route::put('/anggota/update/{id}', 'App\Http\Controllers\AnggotaController@update');
 
 Route::get('/prodi/list', 'App\Http\Controllers\ProdiController@list');
 Route::get('/prodi/detail/{id}', 'App\Http\Controllers\ProdiController@detail');
 Route::get('/prodi/tambah', 'App\Http\Controllers\ProdiController@tambah');
 Route::post('/prodi/store', 'App\Http\Controllers\ProdiController@store');
-
+Route::get('/prodi/edit/{id}', 'App\Http\Controllers\ProdiController@edit');
+Route::put('/prodi/update/{id}', 'App\Http\Controllers\ProdiController@update');
 
 Route::get('peminjamanBuku/list', 'App\Http\Controllers\PeminjamanBukuController@list');
 Route::get('peminjamanBuku/tambah', 'App\Http\Controllers\PeminjamanBukuController@tambah');
