890373675286873a9de32df57a60a708d90912b0
ariq <ariqfms012@students.unnes.ac.id>
2023-07-15 21:54:32 +0700

    add soft delete

--- magangLaravel/app/Models/PeminjamanBuku.php
+++ magangLaravel/app/Models/PeminjamanBuku.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PeminjamanBuku extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $table = 'peminjaman_buku';
     protected $fillable = ['idAnggota', 'idBuku', 'tanggalPinjam', 'tanggalKembali'];
