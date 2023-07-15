36883333488fd338458a3539f13c23fdc6848e25
ariq <ariqfms012@students.unnes.ac.id>
2023-07-15 21:54:07 +0700

    modif bootsrap link

--- magangLaravel/resources/views/master.blade.php
+++ magangLaravel/resources/views/master.blade.php
@@ -3,7 +3,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Belajar Laravel</title>
     <!-- <link rel="stylesheet" type="text/css" href="asset/css/style.css"> -->
@@ -22,7 +22,7 @@
                 <li><a href="/buku/list">Data Buku</a></li>
                 <li><a href="/anggota/list">Data Anggota</a></li>
                 <li><a href="/peminjamanBuku/list">Peminjaman Buku</a></li>
-                <li><a href="/kembalikan-buku">Pengembalian Buku</a></li>
+                <li><a href="/logout">Logout</a></li>
             </ul>
 		</nav> 
         <!--<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -64,5 +64,9 @@
 	<footer>
 		<p>By Ariq</p>
 	</footer>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    </body>
 </body>
 </html>
