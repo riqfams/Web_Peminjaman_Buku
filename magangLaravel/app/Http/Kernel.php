42fe9c53b262101d3fadd201e71a5323d61dbe1c
ariq <ariqfms012@students.unnes.ac.id>
2023-07-20 09:24:39 +0700

    create role, permission, and role has permission

--- magangLaravel/app/Http/Kernel.php
+++ magangLaravel/app/Http/Kernel.php
@@ -64,6 +64,9 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'mustAdmin' => \App\Http\Middleware\MustAdmin::class
+        'mustAdmin' => \App\Http\Middleware\MustAdmin::class,
+        'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
+        'permission' => \Spatie\Permission\Middlewares\PermissionMiddleware::class,
+        'role_or_permission' => \Spatie\Permission\Middlewares\RoleOrPermissionMiddleware::class,
     ];
 }
