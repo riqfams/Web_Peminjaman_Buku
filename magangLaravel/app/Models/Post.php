357383cb5e1cf58af4cf7b05e0142fbd9fd4b422
ariq <ariqfms012@students.unnes.ac.id>
2023-07-20 17:45:33 +0700

    create post model

--- magangLaravel/app/Models/Post.php
+++ magangLaravel/app/Models/Post.php
@@ -4,14 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
     use HasFactory;
 
+    protected $fillabel = [
+        'title', 
+        'post_body'
+    ];
+
     public function image(): MorphOne
     {
         return $this->morphOne(Image::class, 'imageable');
+    }
+
+    public function comments(): MorphMany
+    {
+        return $this->morphMany(Comment::class, 'commentable');
     }
 }
