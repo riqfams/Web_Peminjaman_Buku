e4b24b05cf20d1ebf6ca12693c3b3d383377156d
ariq <ariqfms012@students.unnes.ac.id>
2023-07-20 12:01:18 +0700

    create controller user

---
+++ magangLaravel/app/Http/Controllers/UserController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use App\Events\UserCreated;
+use App\Events\UserDeleted;
+
+class UserController extends Controller
+{
+    public function create(){
+        $user = User::create([
+            'name' => 'Jeni',
+            'role_id' => '1',
+            'email' => 'jeni@gmail.com',
+            'password' => Hash::make('cobajeni'),
+        ]);
+
+        UserCreated::dispatch($user);
+    }
+
+    public function delete($id){
+        $user = User::where('id', $id)->first();
+        $user->delete();
+
+        UserDeleted::dispatch($user);
+    }
+}
