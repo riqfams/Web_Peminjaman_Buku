e769e29533e2e9d684786dbc90ca0e746cd47025
ariq <ariqfms012@students.unnes.ac.id>
2023-07-22 17:03:06 +0700

    add ktp relation to anggota controller and model

--- magangLaravel/app/Http/Controllers/AnggotaController.php
+++ magangLaravel/app/Http/Controllers/AnggotaController.php
@@ -11,6 +11,7 @@ use App\Exports\AnggotaExport;
 use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Facades\Excel;
 use App\Http\Requests\AnggotaRequest;
+use App\Models\Ktp;
 use Illuminate\Support\Facades\Session;
 
 class AnggotaController extends Controller
@@ -18,13 +19,18 @@ class AnggotaController extends Controller
     public function list(Request $request) {
         $keyword = $request->keyword;
 
-        $anggota = Anggota::with('prodi')
+        $anggota = Anggota::with(['prodi', 'ktp'])
                 ->where('nama', 'like', '%'.$keyword.'%')
                 ->orWhere('nim', 'like', '%'.$keyword.'%')
                 ->orWhereHas('prodi', function($query) use($keyword){
                     $query->where('name', 'like', '%'.$keyword.'%');
                 })
+                // ->orWhereHas('ktp', function($query) use($keyword){
+                //     $query->where('nik', 'like', '%'.$keyword.'%');
+                // })
+                // dd($anggota);
                 ->paginate(10);
+                
         return view('anggota/listAnggota',['anggota' => $anggota]); 
     }
     
@@ -40,6 +46,7 @@ class AnggotaController extends Controller
 
     public function store(AnggotaRequest $request){
         $anggota = new Anggota;
+
         $anggota->nama = $request->nama;
         $anggota->kelamin = $request->kelamin;
         $anggota->nim = $request->nim;
@@ -47,6 +54,11 @@ class AnggotaController extends Controller
         $anggota->alamat = $request->alamat;
         $anggota->slug = $request['slug'];
         $anggota->save();
+
+        $ktp = new Ktp();
+        $ktp->anggota_id = $anggota->id;
+        $ktp->nik = $request->nik;
+        $ktp->save();
 
         if($anggota){
             Session::flash('status', 'success');
@@ -56,13 +68,17 @@ class AnggotaController extends Controller
     }
 
     public function edit($slug){
-        $anggota = Anggota::with('prodi')->where('slug', $slug)->first();
-        $prodi = Prodi::where('id', '!=', $anggota->prodi_id)->get(['id', 'name']);
-        return view('anggota/editAnggota', ['anggota' => $anggota, 'prodi' => $prodi]);
+        $anggota = Anggota::with(['prodi', 'ktp'])->where('slug', $slug)->first();
+        // $prodi = Prodi::where('id', '!=', $anggota->prodi_id)->get(['id', 'name']);
+        $prodi = Prodi::get(['id', 'name']);
+        $ktp = Ktp::get(['anggota_id', 'nik']);
+
+        return view('anggota/editAnggota', ['anggota' => $anggota, 'ktp' => $ktp, 'prodi' => $prodi]);
     }
 
-    public function update(AnggotaRequest $request, $slug) {
+    public function update(Request $request, $slug) {
         $anggota = Anggota::where('slug', $slug)->first();
+        // dd($anggota);
         $request['slug'] = Str::slug($request->nama, '-');
 
         $anggota->nama = $request->nama;
@@ -71,7 +87,26 @@ class AnggotaController extends Controller
         $anggota->prodi_id = $request->prodi_id;
         $anggota->alamat = $request->alamat;
         $anggota->slug = $request['slug'];
-        $anggota->save();
+        $anggota->update();
+
+        Ktp::where('anggota_id', $anggota->id)
+            ->update([
+                'nik' => $request->nik
+            ]);
+
+        // $ktp = Ktp::where('anggota_id', $anggota->id)->first();
+        // // dd($ktp);
+        // $ktp->update([
+        //     'anggota_id' => $anggota->id,
+        //     'nik' => $request->nik
+        // ]);
+
+        // $ktp = Ktp::find($anggota->id)->anggota;
+        // dd($ktp);
+
+        // $ktp->anggota_id = $anggota->id;
+        // $ktp->nik = $request->nik;
+        // $ktp->save();
 
         if($anggota){
             Session::flash('status', 'success');
@@ -88,7 +123,7 @@ class AnggotaController extends Controller
     public function destroy($slug){
         $anggota = Anggota::where('slug', $slug)->first();
         $anggota->delete();
-
+        
         if($anggota){
             Session::flash('status', 'success');
             Session::flash('message', 'Data anggota berhasil dihapus');
--- magangLaravel/app/Models/Anggota.php
+++ magangLaravel/app/Models/Anggota.php
@@ -2,18 +2,26 @@
 
 namespace App\Models;
 
-use Cviebrock\EloquentSluggable\Sluggable;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Anggota extends Model
 {
     use HasFactory, SoftDeletes, Sluggable;
 
     protected $table = 'anggota';
-    protected $fillable = ['nama', 'kelamin', 'nim', 'prodi_id', 'alamat', 'slug'];
+    protected $fillable = ['nama', 'kelamin', 'nim', 'nik_id', 'prodi_id', 'alamat', 'slug'];
 
+
+    public function ktp()
+    {
+        return $this->hasOne(Ktp::class);
+    }
+    
     public function prodi()
     {
         return $this->belongsTo(Prodi::class, 'prodi_id');
